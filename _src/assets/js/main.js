5fa5a64f60c1bd8351063b81ba88f090a725a9c3
Laura Ruiz <laura.ruizmazorra@gmail.com>
2019-08-09 09:22:13 +0200

    start favourites array and try painting

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,73 +5,100 @@ const input = document.querySelector('.js-input');
 const favouritesList = document.querySelector('.js-favouritesList');
 const searchesList = document.querySelector('.js-searchesList');
 
-const filmsFormated = [];
+let filmsFormated = [];
+let favouriteFilms = [];
+console.log(favouriteFilms)
 
 function getFilmInfoandPrint(event) {
     event.preventDefault();
     console.log('Hola voy a empezar a buscar');
+    eraseSearchedFilms();
     getDataFromServer();
 }
 
 
-
 function formatData(filmsData) {
-    console.log('Hola estoy formateando')
-    let picture = "";
+    console.log('Hola estoy formateando los datos')
+    let filmPicture = "";
     for (let filmIndex = 0; filmIndex < filmsData.length; filmIndex++) {
         if (filmsData[filmIndex].show.image === null) {
-            picture = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+            filmPicture = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
         } else {
-            picture = `${filmsData[filmIndex].show.image.medium}`
+            filmPicture = `${filmsData[filmIndex].show.image.medium}`
         }
 
         filmsFormated.push({
             name: filmsData[filmIndex].show.name,
-            picture: picture,
+            picture: filmPicture,
             id: filmsData[filmIndex].show.id,
         })
     }
-    console.log(filmsFormated[1])
     return filmsFormated;
 }
 
-
-
 function getDataFromServer() {
-    console.log('Hola voy al servidor')
+    console.log('Hola voy al servidor a por los datos')
     const filmWanted = input.value;
     return fetch(`http://api.tvmaze.com/search/shows?q=${filmWanted}`)
         .then(serverResponse => serverResponse.json())
         .then(serverData => {
             const filmsData = serverData;
             console.log(`Hola estoy en el servidor y te traigo ${filmWanted}`);
-            console.log(filmsData);
             formatData(filmsData);
             printFilms();
         })
-
 }
 
 function printFilms() {
-    debugger;
     console.log('Hola voy a pintar las pelis')
     let htmlCode = ''
     for (const film of filmsFormated) {
-        htmlCode += '<li class="searches-film js-searchedFilm">'
+        htmlCode += `<li class="search_film js-searchedFilm" data-index="${film.id}">`
         htmlCode += `<img src="${film.picture}" alt="${film.name}">`
         htmlCode += `<h3> ${film.name}</h3>`
         htmlCode += '</li>'
+        // filmsShowed.push(film);
     }
     searchesList.innerHTML = htmlCode;
+    listenToSearchedFilms()
 }
 
+function eraseSearchedFilms() {
+    console.log('Hola estoy borrando las búsquedas');
+    filmsFormated = [];
+}
 
+function changeFavourites(event) {
+    const selectedFilm = event.currentTarget;
+    console.log(`Hola ¿le has dado a la peli ${selectedFilm}`)
+    console.log(selectedFilm)
+    selectedFilm.classList.toggle('selected');
+    if (selectedFilm.classList.contains('selected')) {
+        console.log(`Estoy guardando ${selectedFilm} en favoritos`)
+        favouriteFilms.push(selectedFilm)
+    }
+    for (let favouriteIndex = 0; favouriteIndex < favouriteFilms.length; favouriteIndex++) {
+        if (!favouriteFilms[favouriteIndex].classList.contains('selected')) {
+            favouriteFilms.splice(favouriteIndex, 1);
+        }
+    }
+    console.log(`Tus pelis favoritas son: ${favouriteFilms.length}`)
+    printFavourites()
+}
 
+function listenToSearchedFilms() {
+    console.log("Hola voy a escuchar a ver si hacéis click")
+    const filmsShowed = document.querySelectorAll('.js-searchedFilm');
+    for (const film of filmsShowed) {
+        film.addEventListener('click', changeFavourites);
+    }
+}
 
-/*         if (film.picture === null) {
-            htmlCode += `<img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" alt="${film.name}"`
-        } else {
-            htmlCode += `<img src="${film.picture}" alt="${film.name}>`
-        } */
+function printFavourites() {
+    console.log('Hola voy a pintar tus favoritas')
+    for (const film of favouriteFilms) {
+        favouritesList.appendChild(film);
+    }
+}
 
 button.addEventListener('click', getFilmInfoandPrint)
